49a1f275e85783ab09e633b5b9188e5965e2aac8
Raquel Hill <raquelturing@gmail.com>
2021-07-08 17:17:27 -0600

    "Finish warm up"

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,27 +13,28 @@
 // var title = "Harry Potter";
 // var ...
 
-
+var title = "Javascript & Jquery";
+var pages = 400;
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
 
-
+pages = 400 * .75;
 
 // Initialize a new variable "movie", but leave it undefined
 
-
+var movie = ;
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var sequel = `${title} 2.0`;
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
+var isFavorite = true;
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
@@ -41,48 +42,8 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
-
-//------------------------- Conditionals -------------------------//
-
-
-// Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
-// 'Ok, maybe I could read that in a day and a half'
-
-
-
-// Express the following in code: If the variable "isFavorite" is true,
-// log the statement 'My favorite book is [title]!'. Otherwise, log the
-// statement 'I recently read the [title] book'. In either case, the value stored in
-// the "title" variable should be included in the logged statement.
-
-
-
-//------------------------- Arrays -------------------------//
-
-
-// Create a new variable "books" and assign it to an Array of 5 different
-// book titles, represented by their names as Strings.
-
-
-// Write the code to access the 3rd element of the Array, using bracket
-// notation.
-
-
-// Create a new variable "belowTwoHundred" and assign it to an Array of 12
-// different numbers below two hundred.
-
-
-// Write the code to access the 5th element of the array, using bracket notation.
-
-
-//------------------------- Objects -------------------------//
-
-// Create a new variable "dictionary" and assign to it an object literal with two
-// keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
-// the value for hasThesaurus should be false
-
-
-// Write the code to add the key of pageCount, with the value of 720. Use dot
-// notation to do this. DO NOT modify your code from above.
+if (pages < 25) {
+  var isShortStory = true
+} else {
+  var isShortStory = false
+};
