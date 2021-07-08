fe6269f7c50abc5dc93e1d7a393a3ad65029095d
Raquel Hill <raquelturing@gmail.com>
2021-07-08 17:21:14 -0600

    finish shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+};
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      shelf.splice(i, 1)
+    }
+  }
+  return shelf
+};
+
+function listTitles(shelf) {
+  var titles = [];
+  for (i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", ");
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.includes(title)) {
+      return true
+    }
+  }
+  return false
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
