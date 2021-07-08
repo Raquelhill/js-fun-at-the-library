a8c9280dec565a00c27b0e631caf27d925e6bc42
Raquel Hill <raquelturing@gmail.com>
2021-07-08 17:22:13 -0600

    finish Library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,36 @@
+function createLibrary(name, shelves) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+};
 
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
+    return library.shelves.fantasy.push(book)
+  } else if (book.genre === "fiction") {
+    return library.shelves.fiction.push(book)
+  } else {
+    return library.shelves.nonFiction.push(book)
+  }
+};
+
+function checkoutBook(library, title, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === title) {
+      library.shelves[genre].splice(i, 1);
+      return `You have now checked out ${title} from the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
